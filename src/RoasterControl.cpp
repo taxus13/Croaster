07a9258e37a174cc44097d5d8e3a996fc914e098
taxus13 <taxus13@users.noreply.github.com>
2025-06-30 22:44:09 +0200

    start task on demand

--- src/RoasterControl.cpp
+++ src/RoasterControl.cpp
@@ -3,6 +3,7 @@
 #include "HardwareSerial.h"
 #include "PinConfig.h"
 
+
 RoasterControl::RoasterControl(int32_t cycleFan_ms, int32_t cycleHeat_ms) : cycleFan_ms(cycleFan_ms), cycleHeat_ms(cycleHeat_ms)
 {
 }
@@ -11,52 +12,35 @@ void RoasterControl::begin()
 {
     pinMode(SSR_FAN_PIN, OUTPUT);
     pinMode(SSR_HEATER_PIN, OUTPUT);
-    // xTaskCreatePinnedToCore(
-    //     &RoasterControl::startFanSSR,
-    //     "FanSSRTask",
-    //     4096,
-    //     this,
-    //     1,
-    //     NULL,
-    //     1
-    // );
+    digitalWrite(SSR_FAN_PIN, LOW);
+    digitalWrite(SSR_HEATER_PIN, LOW);
 
-    xTaskCreatePinnedToCore(
-        &RoasterControl::startHeaterSSR,
-        "HeaterSSRTask",
-        4096,
-        this,
-        1,
-        NULL,
-        1
-    );
+
 }
 
 void RoasterControl::startFanSSR(void *param)
 {
     RoasterControl* self = static_cast<RoasterControl*>(param);
-    self->SSRTask(SSR_FAN_PIN, self->cycleFan_ms, self->fanLevel);
+    self->SSRTaskFan(SSR_FAN_PIN, self->cycleFan_ms, self->fanLevel);
 }
 
 void RoasterControl::startHeaterSSR(void *param)
 {
     RoasterControl* self = static_cast<RoasterControl*>(param);
-    self->SSRTask(SSR_HEATER_PIN, self->cycleHeat_ms, self->heatLevel);
+    self->SSRTaskHeater(SSR_HEATER_PIN, self->cycleHeat_ms, self->heatLevel);
 }
 
-void RoasterControl::SSRTask(const uint8_t ssr_pin, const int32_t cycle_ms, volatile int32_t& level)
+void RoasterControl::SSRTaskFan(const uint8_t ssr_pin, const int32_t cycle_ms, volatile int32_t& level)
 {
     while (true) {
-        int current_level = level;
 
-        int onTime = (cycle_ms * current_level) / 100;
-        int offTime = cycle_ms - onTime;
+        int onTime = (cycle_ms * level) / 100;
         if (onTime > 0) {
             digitalWrite(ssr_pin, HIGH);
             Serial.printf("Pin %d on\n", ssr_pin);
             vTaskDelay(pdMS_TO_TICKS(onTime));
         }
-
+        int offTime  = (cycle_ms * (100 - level)) / 100;
         if (offTime > 0) {
             digitalWrite(ssr_pin, LOW);
             Serial.printf("Pin %d off\n", ssr_pin);
@@ -64,6 +48,51 @@ void RoasterControl::SSRTask(const uint8_t ssr_pin, const int32_t cycle_ms, vola
         }
     }
 }
+
+void RoasterControl::SSRTaskHeater(const uint8_t ssr_pin, const int32_t cycle_ms, volatile int32_t& level)
+{
+    const int sleep_duration = pdMS_TO_TICKS(100);
+    int last_level = 0;
+    while (true) {
+        last_level = level;
+        int onTime = pdMS_TO_TICKS((cycle_ms * last_level) / 100);
+        if (onTime > 0) {
+            digitalWrite(ssr_pin, HIGH);
+            Serial.printf("Pin %d on\n", ssr_pin);
+            for (int total_sleep_time = 0; total_sleep_time < onTime; total_sleep_time += sleep_duration) {
+                vTaskDelay(sleep_duration);
+                if (last_level != level) {
+                    Serial.println("Level changed");
+                    break;
+                }
+            }
+        }
+
+        last_level = level;
+        int offTime  = (cycle_ms * (100 - last_level)) / 100;
+        if (offTime > 0) {
+            digitalWrite(ssr_pin, LOW);
+            Serial.printf("Pin %d off\n", ssr_pin);
+            for (int total_sleep_time = 0; total_sleep_time < offTime; total_sleep_time += sleep_duration) {
+                vTaskDelay(sleep_duration);
+                if (last_level != level) {
+                    Serial.println("Level changed");
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void RoasterControl::stopTask(TaskHandle_t &handle)
+{
+    if (handle != nullptr) {
+        vTaskDelete(handle);
+        handle = nullptr;
+    }
+}
+
+
 
 void RoasterControl::startRoast()
 {
@@ -77,9 +106,9 @@ void RoasterControl::startCool()
     heatLevel = 0;
     fanLevel = MAX_LEVEL;
     // Immedietly disable the heating element and turn on the fan
+    stopTask(heatTaskHandle);
     digitalWrite(SSR_FAN_PIN, HIGH);
     digitalWrite(SSR_HEATER_PIN, LOW);
-
     Serial.printf("startCool. Current values H=%d,F=%d\n", heatLevel, fanLevel);
 }
 
@@ -88,6 +117,9 @@ void RoasterControl::stopRoast()
     heatLevel = 0;
     fanLevel = 0;
     // Immedietly disable the fan and the heater
+    stopTask(heatTaskHandle);
+    stopTask(fanTaskHandle);
+
     digitalWrite(SSR_FAN_PIN, LOW);
     digitalWrite(SSR_HEATER_PIN, LOW);
     Serial.printf("stopRoast. Current values H=%d,F=%d\n", heatLevel, fanLevel);
@@ -107,9 +139,23 @@ void RoasterControl::setHeat(int32_t percentage)
 
     // If heat was enabled with fan disabled, enable fan now
     if (percentage > 0 && fanLevel < MIN_FAN_LEVEL_FOR_HEAT) {
-        fanLevel = MIN_FAN_LEVEL_FOR_HEAT;
+        Serial.println("Heat turned on without fan, turning it on");
+        setFan(MIN_FAN_LEVEL_FOR_HEAT);
     }
     heatLevel = percentage;
+
+    if (heatTaskHandle == nullptr) {
+        Serial.println("Heat task not running, starting...");
+        xTaskCreatePinnedToCore(
+            &RoasterControl::startHeaterSSR,
+            "HeaterSSRTask",
+            4096,
+            this,
+            1,
+            &heatTaskHandle,
+            1
+        );
+    }
 
     Serial.printf("Received heat percentage. Current values H=%d,F=%d\n", heatLevel, fanLevel);
 }
@@ -130,6 +176,19 @@ void RoasterControl::setFan(int32_t percentage)
         return;
     }
     fanLevel = percentage;
+
+    if (fanTaskHandle == nullptr) {
+        Serial.println("Fan task not running, starting...");
+        xTaskCreatePinnedToCore(
+            &RoasterControl::startFanSSR,
+            "FanSSRTask",
+            4096,
+            this,
+            1,
+            &fanTaskHandle,
+            1
+        );
+    }
 
     Serial.printf("Received fan percentage. Current values H=%d,F=%d\n", heatLevel, fanLevel);
 }
--- src/RoasterControl.h
+++ src/RoasterControl.h
@@ -2,6 +2,9 @@
 
 #include <cstdint>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 static constexpr int32_t MIN_FAN_LEVEL_FOR_HEAT = 50; // Minimum heat level is heat is on
 static constexpr int32_t MIN_HEAT_LEVEL = 0;
 static constexpr int32_t MIN_FAN_LEVEL = 0;
@@ -11,13 +14,18 @@ class RoasterControl {
 private:
     static void startFanSSR(void* param);
     static void startHeaterSSR(void* param);
-    void SSRTask(const uint8_t ssr_pin, const int32_t cycle, volatile int32_t& level);
+    void SSRTaskFan(const uint8_t ssr_pin, const int32_t cycle, volatile int32_t& level);
+    void SSRTaskHeater(const uint8_t ssr_pin, const int32_t cycle, volatile int32_t& level);
+    void stopTask(TaskHandle_t& handle);
 
     volatile int32_t heatLevel = 0;
     volatile int32_t fanLevel = 0;
     const int32_t cycleFan_ms = 0;
     const int32_t cycleHeat_ms = 0;
 
+
+    TaskHandle_t heatTaskHandle = nullptr;
+    TaskHandle_t fanTaskHandle = nullptr;
 public:
     RoasterControl(int32_t cycleFan_ms, int32_t cycleHeat_ms);
     void begin();
